273f44b95b7dc64856f5abffceba704c2f0fc2e6
Kaan-Bas <kaan.bas762@gmail.com>
2025-12-08 23:09:30 +0100

    chore: initialize Spring Boot project and database connection

---
+++ src/test/java/com/kaan/triviagame/TriviaGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaan.triviagame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriviaGameApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
